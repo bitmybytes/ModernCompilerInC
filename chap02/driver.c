798ea5718474c04284638abc58e581d9706eb50a
arunsk <arunsk.tec@gmail.com>
2013-11-10 15:05:44 -0500

    usage changes and adding lexer

--- chap02/driver.c
+++ chap02/driver.c
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
     
     if (argc != 2) {
 
-        fprintf(stderr, "usage: a.out filename\n");
+        fprintf(stderr, "usage: lextext filename\n");
         exit(1);
     }
 
